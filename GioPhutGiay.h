d5ae579062b7c5766f804fae05e13372c5bd8f2b
Linh_hq_uit <huynhquanglinh94@gmail.com>
2025-03-30 22:17:14 +0700

    GioPhutGiay.h

--- GioPhutGiay.h
+++ GioPhutGiay.h
@@ -9,9 +9,15 @@ private:
     int iPhut;
     int iGiay;
 public:
+    // Hàm nhập giờ phút giây. input: giờ phút giây; output: giờ phút giây hợp lệ.
     void Nhap();
+
+    // Hàm xuất giờ phút giây. input:giờ phút giây; output: giờ phút giây theo định dạng.
     void Xuat();
+
+    // Hàm tính giây tiếp theo. input: giờ phút giây đã nhập. output: giây tiếp theo.
+    /* Hướng giải thuật: cộng thêm một giây. Đối với thứ 59 chuyển thành giây 0 và cộng thêm 1 vào phút. 
+    Nếu phút thứ 59 thì chuyển thành phút 0 và cộng 1 thêm vào giờ. Nếu giờ thứ 23 thì chuyển thành giớ thứ 0.*/
     void CongThemMotGiay();
 };
-
 
