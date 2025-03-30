adb98af206b91598d5195df781b79833879086d8
Linh_hq_uit <huynhquanglinh94@gmail.com>
2025-03-30 22:16:33 +0700

    NgayThangNam.h

--- NgayThangNam.h
+++ NgayThangNam.h
@@ -1,6 +1,6 @@
 #pragma once
 #include<bits/stdc++.h>
-using namespace std
+using namespace std;
 
 class NgayThangNam
 {
@@ -9,8 +9,15 @@ private:
     int iThang;
     int iNam;
 public:
+    // Hàm nhập ngày tháng năm. input: ngày tháng năm; output: ngày tháng năm hợp lệ.
     void Nhap();
+
+    // Hàm xuất ngày tháng năm. input:Ngày tháng năm; output: ngày tháng năm theo định dạng.
     void Xuat();
+
+    // Hàm tính ngày tiếp theo. input: ngày tháng năm đã nhập. output: ngày tiếp theo.
+    /* Hướng giải thuật: cộng thêm một ngày. Đối với ngày cuối cùng của tháng thì chuyển 
+    thành ngày 1 và cộng thêm 1 vào tháng. Nếu cộng thêm tháng mà quá 12 thì chuyển tháng thành tháng 1 và cộng thêm vào năm.*/
     void NgayTiepTheo();
 };
 
